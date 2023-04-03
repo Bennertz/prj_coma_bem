f66f338d2ba56768098833a8a24821165ce86c25
Bennertz <126720149+Bennertz@users.noreply.github.com>
2023-04-03 05:44:41 -0300

    Add files via upload

--- src/sp/senai/UnidadeMedida.java
+++ src/sp/senai/UnidadeMedida.java
@@ -1,2 +1,32 @@
-package sp.senai;public class UnidadeMedida {
+package sp.senai;
+
+public class UnidadeMedida {
+    private int idunidade_medida; // Variável que armazena o identificador da unidade de medida
+    private  String nome_unidade_medida; // Variável que armazena o nome da unidade de medida
+
+    // Construtor da classe UnidadeMedida
+    public UnidadeMedida(int idunidade_medida, String nome_unidade_medida) {
+        this.idunidade_medida = idunidade_medida;
+        this.nome_unidade_medida = nome_unidade_medida;
+    }
+
+    // Método para recuperar o identificador da unidade de medida
+    public int getIdunidade_medida() {
+        return idunidade_medida;
+    }
+
+    // Método para alterar o identificador da unidade de medida
+    public void setIdunidade_medida(int idunidade_medida) {
+        this.idunidade_medida = idunidade_medida;
+    }
+
+    // Método para recuperar o nome da unidade de medida
+    public String getNome_unidade_medida() {
+        return nome_unidade_medida;
+    }
+
+    // Método para alterar o nome da unidade de medida
+    public void setNome_unidade_medida(String nome_unidade_medida) {
+        this.nome_unidade_medida = nome_unidade_medida;
+    }
 }
